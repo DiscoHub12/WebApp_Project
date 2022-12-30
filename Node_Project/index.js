23691e06108743f4e006a183ad8f8b285c09126d
Sofia <scattolinisofia26@gmail.com>
2022-12-30 18:25:22 +0100

    update index.js

--- Node_Project/index.js
+++ Node_Project/index.js
@@ -1,8 +1,56 @@
-const express = require('express');
-const app = express()
+const express = require('express'); 
+const app = express(); 
 
-app.get('/', function(req, res){
-    res.send("Hello World"); 
+//ESEMPIO 1 LINK
+app.get('/home', function(req, res){
+    var counter = 10; 
+    res.send('Pagina Home');
 });
 
+//ESEMPIO 2 LINK
+app.get('/contatti', function(req, res){
+    res.send('Pagina Contatti');
+});
+
+//ESEMPIO 3 LINK
+app.get('/informazioni', function(req,res){
+    res.send('Pagina Informazioni')
+})
+
+
+//ESEMPIO 4 LINK
+app.get('/login', function(req, res){
+    res.send('Pagina Iscrizioni')
+})
+
+
+//ESEMPIO PRATICO DI INVIO DATI
+/**
+ * Questa chiamata ci ritorna i post di instragram
+ * che vogliamo vedere. Quindi ci dovrà ritornare un qualcosa
+ * di array. 
+ */
+app.get('/api/feed', function(req,res){
+    //connessione al db
+    //avrò i miei amici da visualizzare nei feed
+    //alloco come risultato un array, che simula un risultato
+    //di una select.
+    var myFeed = [
+         {
+            id: '1', 
+            name: 'Pippo', 
+            like: '123'
+         },
+         {
+            id: '2', 
+            name: 'Pluto', 
+            like: '2'
+         },
+    ];
+
+    //Devo inviare questa cosa al Front-end che mi ha fatto la chiamata (inviare del json)
+    res.json(myFeed);
+})
+
+console.log("Server start at port : 3000 ...")
 app.listen(3000);
