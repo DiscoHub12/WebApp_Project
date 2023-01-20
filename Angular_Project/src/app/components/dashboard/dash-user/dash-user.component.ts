6b58f075f5222386642778a56f4009e74ec6cac9
DiscoHub12 <discordo12di@gmail.com>
2023-01-20 18:42:14 +0100

    [INIT IMPLEMENTATION] Dashboard User.

--- Angular_Project/src/app/components/dashboard/dash-user/dash-user.component.ts
+++ Angular_Project/src/app/components/dashboard/dash-user/dash-user.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dash-user',
   templateUrl: './dash-user.component.html',
   styleUrls: ['./dash-user.component.scss']
 })
-export class DashUserComponent {
+export class DashUserComponent implements OnInit {
 
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
