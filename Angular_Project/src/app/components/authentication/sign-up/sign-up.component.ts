607ef4fdd53d3946aee9cd0a776784ef58a830c6
Sofia <scattolinisofia26@gmail.com>
2023-01-20 16:22:12 +0100

    Gestita registrazione utente

--- Angular_Project/src/app/components/authentication/sign-up/sign-up.component.ts
+++ Angular_Project/src/app/components/authentication/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators} from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { environment } from 'src/environments/environments';
 
 @Component({
@@ -13,7 +13,7 @@ export class SignUpComponent {
 
   public signupForm !: FormGroup;
 
-  disabled = true;
+  data : any;
 
 
 constructor(private formBuilder : FormBuilder, private httpClient : HttpClient, private router : Router) {
@@ -23,8 +23,8 @@ ngOnInit() : void {
   this.signupForm = this.formBuilder.group({
     fullname : ['', Validators.required],
     lastname : ['', Validators.required],
-    email : ['', Validators.required],
-    password : ['', Validators.required]
+    email : ['', [Validators.email, Validators.required]],
+    password : ['', [Validators.required, Validators.minLength(6)]]
   },
   )
 }
@@ -34,8 +34,21 @@ signUp() {
   const lastname = this.signupForm.value.lastname;
   const email = this.signupForm.value.email;
   const password = this.signupForm.value.password;
-  this.disabled = false;
-  this.httpClient.post(environment.baseUrl + "/signupUser", {fullnameUser : fullname, lastnameUser : lastname, emailUser : email, passwordUser : password}).subscribe();
+  this.httpClient.post(environment.baseUrl + "/signupUser", {fullnameUser : fullname, lastnameUser : lastname, emailUser : email, passwordUser : password}).subscribe(
+    res =>{
+      this.data=res;
+      if(this.data.status==201) {
+        alert("Registrazione avvenuta con successo");
+        this.toLogin();
+      }else {
+        alert("Registrazione non avvenuta con successo");
+      }
+    }
+  );
 }
 
+toLogin() {
+  this.signupForm.reset();
+  this.router.navigate(['login']);
+}
 }
