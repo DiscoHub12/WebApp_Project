03bc20c058bfef18b1e256932fbbc374a38d537a
DiscoHub12 <discordo12di@gmail.com>
2023-01-23 17:35:05 +0100

    [UPDATE] Directory and file into Angular Frontend.

---
+++ Angular_Project/src/app/components/authentication/auth_employee/login-emp/login-emp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-emp',
+  templateUrl: './login-emp.component.html',
+  styleUrls: ['./login-emp.component.scss']
+})
+export class LoginEmpComponent {
+
+}
