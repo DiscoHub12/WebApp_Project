360f205dcbd1212d9115eb05440d9bd360379a30
DiscoHub12 <discordo12di@gmail.com>
2023-01-25 19:13:21 +0100

    [IMPLEMENT] Employee.js model for Sequelize rapresent table into DataBase

--- Node_Project/models/employee.js
+++ Node_Project/models/employee.js
@@ -1,2 +1,32 @@
-const { DataTypes } = require("sequelize");
-//Todo implementare
+const {Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = require('../config/database'); 
+
+class Employee extends Model {}
+
+Employee.init({
+     //Model attributes are defined here: 
+     id: {
+        type: DataTypes.INTEGER(3),
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    }, 
+    codice : {
+        type: DataTypes.INTEGER(5), 
+        allowNull: false,
+    }, 
+    salt: {
+        type: DataTypes.CHAR(200), 
+        allowNull: false, 
+    }, 
+    password: {
+        type: DataTypes.CHAR(200), 
+        allowNull: false, 
+    }
+
+}, {
+    sequelize, 
+    modelName: 'Employee'
+});
+
+module.exports = Employee;
