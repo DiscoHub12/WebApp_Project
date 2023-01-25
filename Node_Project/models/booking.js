c5a3357931d314de8e5ba6de793ea06863ed2dd3
Sofia <scattolinisofia26@gmail.com>
2023-01-25 12:36:10 +0100

    Implement model booking.js

--- Node_Project/models/booking.js
+++ Node_Project/models/booking.js
@@ -1,2 +1,27 @@
+const sequelize = require("sequelize");
 const { DataTypes } = require("sequelize");
-//Todo implementare
+
+module.exports = (sequelize, Sequelize) => {
+    const Booking = sequelize.define( "booking" , {
+        id : {
+            type : DataTypes.INTEGER(4),
+            autoIncrement : true,
+            primaryKey : true,
+            allowNull: false
+        },
+        idUtente : {
+            type: DataTypes.INTEGER(4),
+            allowNull: false,
+        },
+        dataPrenotazione : {
+            type : DataTypes.DATE,
+            allowNull : false,
+        },
+        completata : {
+            type : DataTypes.INTEGER(1),
+            allowNull : false
+        }
+    });
+
+    return Booking;
+}
