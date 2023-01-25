7650a3620cab06f0582e75371525e75082147152
Sofia <scattolinisofia26@gmail.com>
2023-01-25 12:38:05 +0100

    implement routes booking - booking.routes.js

---
+++ Node_Project/routes/booking.routes.js
@@ -0,0 +1,24 @@
+module.exports = app => {
+    const booking = require('../controllers/booking_controller.js'); 
+
+    var router = require('express').Router(); 
+
+    //Init the routes : 
+    router.post("/create", booking.create);
+
+    router.post("/delete/:id", booking.delete);
+
+    router.post("/deleteAll", booking.deleteAll);
+
+    router.get("/find/:id", booking.find);
+
+    router.get("/findAll", booking.findAll);
+
+    router.get("./findAllCompleted", booking.findAllCompleted)
+
+    router.post("/update", booking.update);
+
+
+    //Route for this module : 
+    app.use('/api/booking', router);
+}
