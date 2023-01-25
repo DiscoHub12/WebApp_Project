d938650153224b239eccee28d96c7a618bddf9b5
DiscoHub12 <discordo12di@gmail.com>
2023-01-25 19:14:49 +0100

    [IMPLEMENT] Routes fot Card.

--- Node_Project/routes/card.routes.js
+++ Node_Project/routes/card.routes.js
@@ -3,8 +3,21 @@ module.exports = app => {
 
     var router = require('express').Router(); 
 
-    //Init the routes : 
     router.post("/create", card.create);
+
+    router.post("/delete/:id", card.delete); 
+
+    router.post("/addPoints/:id", card.addPoints); 
+
+    router.post("/addPointsAll", card.addPointsAll); 
+
+    router.post("/removePoints/:id", card.removePoints);
+
+    router.post("/removePointsAll", card.removePointsAll);
+
+    router.get("/find/:id", card.find); 
+
+    router.get("/findAll", card.findAll);
 
     //Route for this module : 
     app.use('/api/card', router);
