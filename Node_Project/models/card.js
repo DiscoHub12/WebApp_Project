7bef4628147151bcb7f9843836362dfd11f5a029
DiscoHub12 <discordo12di@gmail.com>
2023-01-25 19:12:53 +0100

    [IMPLEMENT] Card.js model for Sequelize rapresent table into DataBase

--- Node_Project/models/card.js
+++ Node_Project/models/card.js
@@ -1,27 +1,31 @@
-const { DataTypes } = require("sequelize"); 
+const {Sequelize, DataTypes, Model} = require('sequelize');
+const sequelize = require('../config/database'); 
 
-module.exports = (sequelize, Sequelize) => {
-    const Card = sequelize.define("card", {
-        id: {
-            type: DataTypes.INTEGER(3),
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false
-        },
-        idUtente: {
-            type: DataTypes.INTEGER(4),
-            allowNull: false,
-        },
-        codice: {
-            type: DataTypes.CHAR(10),
-            allowNull: false,
-        },
-        punti: {
-            type: DataTypes.INTEGER(4),
-            allowNull: false, 
-            defaultValue: 0
-        },
-    });
+class Card extends Model {}
 
-    return Card;
-}
+Card.init({
+     //Model attributes are defined here: 
+     id: {
+        type: DataTypes.INTEGER(3),
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    idUtente: {
+        type: DataTypes.INTEGER(4),
+        allowNull: false,
+    },
+    codice: {
+        type: DataTypes.CHAR(10),
+        allowNull: false,
+    },
+    punti: {
+        type: DataTypes.INTEGER(4),
+        allowNull: false, 
+        defaultValue: 0
+    }},{
+    sequelize, 
+    modelName: 'Card'
+}); 
+
+module.exports = Card; 
