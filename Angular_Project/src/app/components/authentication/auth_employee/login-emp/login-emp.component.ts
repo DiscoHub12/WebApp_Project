77542a71ea923ec10b6fd2ad18d615427cd725eb
DiscoHub12 <discordo12di@gmail.com>
2023-01-27 16:27:32 +0100

    [INIT] Implementation ..ts file for employee login and registration.

--- Angular_Project/src/app/components/authentication/auth_employee/login-emp/login-emp.component.ts
+++ Angular_Project/src/app/components/authentication/auth_employee/login-emp/login-emp.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-emp',
   templateUrl: './login-emp.component.html',
   styleUrls: ['./login-emp.component.scss']
 })
-export class LoginEmpComponent {
+export class LoginEmpComponent implements OnInit {
 
+  //Attributes-variables for thid component: 
+  public loginForm !: FormGroup;
+
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      code: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  //Method for this Component:
+  onSubmit() {
+
+  }
 }
--- Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
+++ Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup-emp',
   templateUrl: './signup-emp.component.html',
   styleUrls: ['./signup-emp.component.scss']
 })
-export class SignupEmpComponent {
+export class SignupEmpComponent implements OnInit {
+
+  public signupForm !: FormGroup;
+
+  data: any;
+
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      fullname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    },
+    )
+  }
 
 }
