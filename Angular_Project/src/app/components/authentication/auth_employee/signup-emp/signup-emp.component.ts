29871ffb1731627aa1d9bc075b7dad8dc7f49cbf
DiscoHub12 <discordo12di@gmail.com>
2023-01-28 11:14:37 +0100

    [CONTINUE] Implementation for signup Employee.

--- Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
+++ Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environments';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-signup-emp',
@@ -14,18 +16,63 @@ export class SignupEmpComponent implements OnInit {
 
   data: any;
 
+  isSubscribe = false;
 
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router) {
+
+  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router, public dialog: MatDialog) {
   }
 
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
       fullname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      email: ['', [Validators.email, Validators.required]],
+      code: ['', [Validators.required]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     },
     )
   }
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(DialogAnimationsExampleDialog, {
+      width: '300px',
+      height: '150px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
+
+  onSubmit() {
+    const fullname = this.signupForm.value.fullname;
+    const code = this.signupForm.value.code;
+    const password = this.signupForm.value.password;
+    this.httpClient.post(environment.baseUrl + "/employee/create", { nameEmployee: fullname, code: code, passwordEmployee: password }).subscribe(
+      res => {
+        this.data = res;
+        if (this.data.status == 201) {
+          alert("Registrazione avvenuta con successo");
+          console.log(this.data);
+          this.openDialog('0ms', '0ms');
+          this.toLogin();
+        } else {
+          alert("Registrazione non avvenuta con successo");
+        }
+      }
+    );
+
+  }
+
+
+  toLogin() {
+    this.signupForm.reset();
+    this.data.reset();
+    this.router.navigate(['login-emp']);
+  }
+}
+
+@Component({
+  selector: 'app-signup-emp-dialog',
+  templateUrl: 'dialog.html',
+  styleUrls: ['./signup-emp.component.scss'] //Mettere un file singolo per il css
+})
+export class DialogAnimationsExampleDialog {
+  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>) { }
 }
