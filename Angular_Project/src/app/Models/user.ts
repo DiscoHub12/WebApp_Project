c27696e6ee506e05c1c89e9817c9d14e269afc1b
Sofia <scattolinisofia26@gmail.com>
2023-01-29 18:30:11 +0100

    Implement user.ts

--- Angular_Project/src/app/Models/user.ts
+++ Angular_Project/src/app/Models/user.ts
@@ -1,3 +1,24 @@
 export class User {
+    id : Number;
+    nome : String;
+    cognome : String;
+
+    constructor(id : Number, nome : String, cognome : String) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getNome() {
+        return this.nome;
+    }
+
+    getCognome() {
+        return this.cognome;
+    }
     
 }
