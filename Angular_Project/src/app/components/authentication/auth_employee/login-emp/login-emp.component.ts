eeccdba6bb2867f304ac46179227100301044937
DiscoHub12 <discordo12di@gmail.com>
2023-01-31 19:15:39 +0100

    [CONTINUE IMPLEMENTATION] for Employee Login.

--- Angular_Project/src/app/components/authentication/auth_employee/login-emp/login-emp.component.ts
+++ Angular_Project/src/app/components/authentication/auth_employee/login-emp/login-emp.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environments';
 import { Employee } from 'src/app/Models/employee';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login-emp',
@@ -16,11 +17,12 @@ export class LoginEmpComponent implements OnInit {
   //Attributes-variables for thid component: 
   public loginForm !: FormGroup;
 
+  //Data for associate the response
   data: any;
 
   employeeLogged: Employee | undefined;
 
-  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private router: Router) { }
+  constructor(private httpClient: HttpClient, private formBuilder: FormBuilder, private router: Router, private authService : AuthService) { }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
@@ -33,12 +35,13 @@ export class LoginEmpComponent implements OnInit {
   onSubmit() {
     const code = this.loginForm.value.code;
     const password = this.loginForm.value.password;
-    this.httpClient.post(environment.baseUrl + '/loginUser', { code: code, passwordUser: password }).subscribe(
+    this.httpClient.post(environment.baseUrl + '/loginEmployee', { code: code, passwordUser: password }).subscribe(
       res => {
         this.data = res;
         if (this.data.status == 201) {
           alert('Login successfully.');
           this.employeeLogged = new Employee(this.data.id, this.data.nome, this.data.codice, this.data.restrizioni);
+          this.authService.setUserData(this.employeeLogged);
           this.router.navigate(['userDashboard']);
         } else {
           alert("Password not correct.");
