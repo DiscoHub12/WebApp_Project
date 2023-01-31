a13bea3082cdb2914c6b76bad0497b5348a7ffce
DiscoHub12 <discordo12di@gmail.com>
2023-01-31 19:20:57 +0100

    [RESOLVE] Conflict

--- Angular_Project/src/app/app-routing.module.ts
+++ Angular_Project/src/app/app-routing.module.ts
@@ -7,8 +7,6 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { InfoComponent } from './components/pages/info/info.component';
 import { LoginComponent } from './components/authentication/auth_user/login/login.component';
 import { SignUpComponent } from './components/authentication/auth_user/sign-up/sign-up.component';
-import { DashUserComponent } from './components/dashboard/dash-user/dash-user.component';
-import { DashEmployeeComponent } from './components/dashboard/dash-employee/dash-employee.component';
 import { ProductsComponent } from './components/pages/products/products.component';
 import { LoginEmpComponent } from './components/authentication/auth_employee/login-emp/login-emp.component';
 import { SignupEmpComponent } from './components/authentication/auth_employee/signup-emp/signup-emp.component';
@@ -23,8 +21,6 @@ const routes: Routes = [
   {path: 'signup-user', component: SignUpComponent},
   {path: 'login-emp', component: LoginEmpComponent}, 
   {path: 'signup-emp', component: SignupEmpComponent},
-  {path: 'userDashboard', component: DashUserComponent}, 
-  {path: 'employeeDashboard', component: DashEmployeeComponent}, 
 ];
 
 @NgModule({
