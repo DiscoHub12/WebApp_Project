c236d853d38c94abe997e611cd537831bc0ce3b0
DiscoHub12 <discordo12di@gmail.com>
2023-01-31 19:13:42 +0100

    [ADD] Route for Login and Logout about Employee and User.

--- Node_Project/routes/user.routes.js
+++ Node_Project/routes/user.routes.js
@@ -1,23 +1,32 @@
 module.exports = app => {
 
+    //Require user controller for methods
     const user = require('../controllers/user_controller');
 
+    //Require express connection 
     var router = require('express').Router();
 
-    //Init the routes :
+    //Router for Signup Login
     router.post("/signup", user.create);
 
+    //Route for User login
+    router.post("/login", user.login);
+
+    //Route for delete User by Id
     router.post("/delete/:id", user.delete);
 
+    //Route for delete all User
     router.post("/deleteAll", user.deleteAll);
 
+    //Route for find User by Id
     router.get("/find/:id", user.find);
 
+    //Route for find all User
     router.get("/findAll", user.findAll);
 
+    //Route for update User account
     router.post("/update/:id", user.update);
 
-    
     //Route for this module : 
     app.use('/api/user', router);
 
