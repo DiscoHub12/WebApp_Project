f9679cd89ee8c4b0fb703ffbc77e4722811508af
Sofia <scattolinisofia26@gmail.com>
2023-01-31 19:01:07 +0100

    [ADD] auth guard to protect routes

--- Angular_Project/src/app/components/authentication/auth.service.ts
+++ Angular_Project/src/app/components/authentication/auth.service.ts
@@ -2,6 +2,7 @@ import { Employee } from "src/app/Models/employee";
 import { User } from "src/app/Models/user";
 
 export class AuthService {
+    [x: string]: any;
     private userData: User | Employee | undefined;
   
     setUserData(data: User | Employee) {
@@ -10,5 +11,10 @@ export class AuthService {
   
     getUserData() {
       return this.userData;
+    }
+
+    //Controlla se il token è scaduto e restituisce true o false
+    isLoggedIn() {
+      return !!localStorage.getItem('token');
     }
 }
