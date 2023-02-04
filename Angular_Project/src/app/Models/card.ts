bc84e8ee78e453e055d62dcbed34684c613deaae
Sofia <scattolinisofia26@gmail.com>
2023-02-04 12:32:18 +0100

    Implement card class

--- Angular_Project/src/app/Models/card.ts
+++ Angular_Project/src/app/Models/card.ts
@@ -1,3 +1,18 @@
 export class Card{
-    
+
+    points : Number;
+
+
+    constructor(points : Number){
+        this.points = points;
+    }
+
+    getPoints() {
+        return this.points;
+    }
+
+    setPoints(points : Number) {
+        this.points = points;
+    }
+
 }
