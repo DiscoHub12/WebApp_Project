a10826140ba54708489c1cc97e50a0ac5f8830aa
DiscoHub12 <discordo12di@gmail.com>
2023-02-06 10:06:18 +0100

    [IMPLEMENT] Service for User logged for send this data in other Components.

---
+++ Angular_Project/src/app/service/user.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { User } from '../Models/user';
+import { Employee } from '../Models/employee';
+
+//Send data to root path.
+@Injectable({
+    providedIn : 'root'
+})
+
+/**
+ * Class that acts as a Service to send a 
+ * user logged in to other project components. 
+ * It will be used to send the logged in user to the dashboard.
+ */
+export class UserService{
+
+    /**
+     * Variable to which a user logged 
+     * in with the set method will be associated.
+     */
+    private user : User | Employee | undefined; 
+
+    //Constructor for this component.
+    constructor(){}
+
+    /**
+     * Method that allows you to set 
+     * a user logged in to the above variable. 
+     * This method is called in the Login component of both users.
+     */
+    setUser(user: User | Employee){
+        this.user = user; 
+    }
+
+    /**
+     * Method that allows you to 
+     * take the User object.
+     */
+    getUser(){
+        return this.user;
+    }
+}
