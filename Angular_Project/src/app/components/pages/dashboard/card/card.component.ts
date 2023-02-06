b9a78a87ab2f6b8e1dbbd24d69fc4968a1fdaf25
DiscoHub12 <discordo12di@gmail.com>
2023-02-06 16:52:26 +0100

    [INIT] Card Pages

--- Angular_Project/src/app/components/pages/dashboard/card/card.component.ts
+++ Angular_Project/src/app/components/pages/dashboard/card/card.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/Models/employee';
+import { UserService } from 'src/app/service/user.service';
+import { environment } from 'src/environments/environments';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
+
+  userType = new Employee(12, 'John', "Codice", 1);
+
+
+  clientCards: any;
+
+  data: any;
+
+  printCard = false; 
+
+  constructor(
+    private httpClient: HttpClient,
+    private userService: UserService
+  ) { }
+
+
+  ngOnInit(): void {
+    //this.userType = this.userService.getUser();
+  }
+
+
+  getCardsUser() {
+    this.httpClient.get<any>(`${environment.baseUrl}/card/findAll`).subscribe(
+      response => {
+        this.data = response;
+        if (this.data.status == 201) {
+          this.clientCards = JSON.parse(JSON.stringify(this.data.data));
+        } else {
+          alert("error");
+        }
+      }, err => {
+        alert("Something went wrong");
+      });
+  }
+
+  checkCards(){
+    if(this.printCard == false){
+
+    }else {
+      this.getCardsUser();
+    }
+  }
+
 
 }
+
