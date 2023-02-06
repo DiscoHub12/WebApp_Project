eac85bb6d6ae8654ca9d36b12bbd2e55b8277ef0
DiscoHub12 <discordo12di@gmail.com>
2023-02-06 10:10:01 +0100

    [CONTINUE] Implementation for Signup component for Employee and User

--- Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
+++ Angular_Project/src/app/components/authentication/auth_employee/signup-emp/signup-emp.component.ts
@@ -7,54 +7,71 @@ import { environment } from 'src/environments/environments';
 @Component({
   selector: 'app-signup-emp',
   templateUrl: './signup-emp.component.html',
-  styleUrls: ['./signup-emp.component.scss']
+  styleUrls: ['./signup-emp.component.scss'], 
+  
 })
+
+
 export class SignupEmpComponent implements OnInit {
 
+  //variable for FormGroup
   public signupForm !: FormGroup;
 
-  data: any;
+  //Variable for associate the response Http when Registration is successful
+  response : any;
 
-  isSubscribe = false;
-
-
+  /**
+   * Constructor for this Component.
+   * @param formBuilder the FormBuilder for all Form and check data in HTML. 
+   * @param httpClient the HttpClient module for this component.
+   * @param router the Router module for this component for navigation.
+   */
   constructor(private formBuilder: FormBuilder, private httpClient: HttpClient, private router: Router) {
   }
 
+  //NgOnInit implementation
   ngOnInit(): void {
     this.signupForm = this.formBuilder.group({
       fullname: ['', Validators.required],
       code: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]], 
+      restriction: ['', Validators.required],
     },
     )
   }
 
-
-  onSubmit() {
-    const fullname = this.signupForm.value.fullname;
+  /**
+   * Method for registration the new Employee.
+   */
+  onSubmit(){
+    const fullName = this.signupForm.value.fullname;
     const code = this.signupForm.value.code;
     const password = this.signupForm.value.password;
-    this.httpClient.post(environment.baseUrl + "/employee/registration", { nameEmployee: fullname, code: code, passwordEmployee: password }).subscribe(
-      res => {
-        this.data = res;
-        if (this.data.status == 201) {
-          alert("Registrazione avvenuta con successo");
-          console.log(this.data);
-          this.toLogin();
-        } else {
-          alert("Registrazione non avvenuta con successo");
+    let restriction; 
+    if(this.signupForm.value.restriction == true){
+      restriction = 0;
+    }else restriction = 1;
+    this.httpClient.post(`${environment.baseUrl}/employee/registration`, {
+      nameEmployee : fullName,
+      code : code,
+      passwordEmployee : password,
+      restrizioni : 0
+    }).subscribe(
+      response => {
+        this.response = response;
+        if(this.response.status == 201){
+          alert("Registrazione avvenuta con successo.");
+          this.router.navigate(['login-emp']);
+          this.resetForm();
+        }else {
+          alert("Registrazione non riuscita.");
         }
-      }
-    );
-
+      });
   }
 
-
-  toLogin() {
+  //This method reset the form value of FormGroup.
+  resetForm() {
     this.signupForm.reset();
-    this.data.reset();
-    this.router.navigate(['login-emp']);
   }
 }
 
