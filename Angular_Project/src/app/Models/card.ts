3c9bc0b846316177f7f013b361a212213dbca8fa
DiscoHub12 <discordo12di@gmail.com>
2023-02-13 10:40:27 +0100

    [UPDATE] Card Model.

--- Angular_Project/src/app/Models/card.ts
+++ Angular_Project/src/app/Models/card.ts
@@ -1,15 +1,22 @@
+
 export class Card{
     id : Number;
     idUtente : Number; 
     codice : String; 
     punti : Number; 
+    owner : {
+        nome: String, 
+        cognome:String,
+    }
 
 
-    constructor(id: Number, idUtente : Number, codice : String, punti : Number){
+    constructor(id: Number, idUtente : Number, codice : String, punti : Number, owner : any){
+
         this.id = id;
         this.idUtente = idUtente;
         this.codice = codice;
         this.punti = punti;
+        this.owner = owner; 
     }
 
     getPunti() {
