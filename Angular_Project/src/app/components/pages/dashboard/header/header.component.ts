5ad43e31554dc695c60e08089eed7356087c09ae
Sofia <scattolinisofia26@gmail.com>
2023-02-13 12:35:35 +0100

    Changes component sidenav and header

--- Angular_Project/src/app/components/pages/dashboard/header/header.component.ts
+++ Angular_Project/src/app/components/pages/dashboard/header/header.component.ts
@@ -1,6 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/Models/employee';
 import { User } from 'src/app/Models/user';
 import { UserService } from 'src/app/service/user.service';
+import { environment } from 'src/environments/environments';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +14,18 @@ import { UserService } from 'src/app/service/user.service';
 export class HeaderComponent {
   @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
 
-  userType : any;
+  userType: any;
 
-  constructor(private authUser : UserService){}
+  //Variable for associate the response Http when Logout is successful.
+  response: any;
 
-  ngOnInit () {
+  constructor(
+    private authUser: UserService, 
+    private httpClient : HttpClient,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
     this.userType = this.authUser.getUser();
   }
 
@@ -23,5 +34,38 @@ export class HeaderComponent {
   }
 
 
+  //Logout method -- finire 
+  logout() {
+    if (this.userType instanceof User) {
+      this.httpClient.post(`${environment.baseUrl}/user/logout`, {}).subscribe(
+        response => {
+          this.response = response;
+          if (this.response.status == 201) {
+            alert("Logout Successfully");
+            this.router.navigate(['']);
+          } else {
+            alert ("Logout Failed");
+          }
+        }, err => {
+          alert("Logout Failed");
+        }
+      );
+    }
+    if (this.userType instanceof Employee) {
+      this.httpClient.post(`${environment.baseUrl}/employee/logout`, {}).subscribe(
+        response => {
+          this.response = response;
+          if (this.response.status == 201) {
+            alert("Logout Successfully");
+            this.router.navigate(['']);
+          } else {
+            alert ("Logout Failed");
+          }
+        }, err => {
+          alert("Logout Failed");
+        }
+      );
+    }
+  }
 
 }
