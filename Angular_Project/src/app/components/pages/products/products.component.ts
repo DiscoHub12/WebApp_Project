10f41a0ce739eaf53dbd3841da4a6ba0da2c6652
DiscoHub12 <discordo12di@gmail.com>
2023-02-14 17:36:21 +0100

    [INIT-IMPLEMENTATION] for Products component in Angular.

--- Angular_Project/src/app/components/pages/products/products.component.ts
+++ Angular_Project/src/app/components/pages/products/products.component.ts
@@ -7,4 +7,60 @@ import { Component } from '@angular/core';
 })
 export class ProductsComponent {
 
+  products = [
+    {
+      name: 'Prodotto 1',
+      description: 'Questa è una descrizione del prodotto 1',
+      price: 10,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 2',
+      description: 'Questa è una descrizione del prodotto 2',
+      price: 20,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 3',
+      description: 'Questa è una descrizione del prodotto 3',
+      price: 30,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    } ,
+    {
+      name: 'Prodotto 4',
+      description: 'Questa è una descrizione del prodotto 1',
+      price: 10,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 5',
+      description: 'Questa è una descrizione del prodotto 2',
+      price: 20,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 6',
+      description: 'Questa è una descrizione del prodotto 3',
+      price: 30,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },  {
+      name: 'Prodotto 7',
+      description: 'Questa è una descrizione del prodotto 1',
+      price: 10,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 8',
+      description: 'Questa è una descrizione del prodotto 2',
+      price: 20,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    },
+    {
+      name: 'Prodotto 9',
+      description: 'Questa è una descrizione del prodotto 3',
+      price: 30,
+      imageUrl: 'https://via.placeholder.com/500x325'
+    }
+  ]
+
 }
