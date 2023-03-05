15ffaa1129c406ea08adb637ac9aeeda1ba7db4a
DiscoHub12 <discordo12di@gmail.com>
2023-03-05 19:47:06 +0100

    [IMPLEMENT] App component: html, scss, ts. Then resolve Dependencies in the Project.

--- Angular_Project/src/app/app.component.ts
+++ Angular_Project/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { environment } from 'src/environments/environments';
+import { NavigationEnd, Router } from '@angular/router';
 
 
 @Component({
@@ -8,7 +9,36 @@ import { environment } from 'src/environments/environments';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  //Web title:
   title = 'Angular_Project';
+
+  //Variables that indicates if the page is standard or dashboard
+  showMenuBar = true;
+
+  /**
+   * Constructor for AppComponent.
+   * @param router the Router module.
+   */
+  constructor(
+    private router: Router
+  ) { }
+
+
+  /**
+   * NgOnInit implementation, that check if the User stay
+   * on home or standard pages, or dashboard.
+   */
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/dashboard/user' || event.url === '/dashboard/employee') {
+          this.showMenuBar = false;
+        } else {
+          this.showMenuBar = true;
+        }
+      }
+    })
+  }
 
 }
